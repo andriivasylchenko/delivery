ac9c9e57b473eba50e92c0c8ffcb05fdd14e964e
andriivasylchenko <vasylchenko.andrii@gmail.com>
2017-06-17 14:21:35 +0200

    initial app

---
+++ deliveryApp/src/providers/news/news-provider.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class NewsProvider {
+  data: any = null;
+
+  constructor(public http: Http) { }
+
+  load() {
+    console.log('---> called NewsProvider load');
+
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      this.http.get('http://10.0.1.9:4567/news')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          console.debug('News load data ', this.data.news);
+          resolve(this.data.news);
+        });
+    });
+  }
+}
+
