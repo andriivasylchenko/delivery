c058e7059efcf70b91d9224e1bef17b44700cf46
andriivasylchenko <vasylchenko.andrii@gmail.com>
2017-06-18 18:55:47 +0200

    lab 2.73 results

---
+++ deliveryApp/src/providers/employee/employee-provider.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class EmployeeProvider {
+  data: any = null;
+
+  constructor(public http: Http) { }
+
+  load() {
+    console.log('---> called EmployeeProvider load');
+
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      this.http.get('http://localhost:4567/employees')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          console.debug('Employee load data ', this.data.results);
+          resolve(this.data.results);
+        });
+    });
+  }
+}
+
